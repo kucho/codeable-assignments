84c13a19e6197f32c531a4eab964dbb4e7c81a32
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-03-25 16:53:51 -0500

    cash done

---
+++ Week 2/Day 3/cash.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+int change_counter(int change)
+{
+    int count = 0;
+
+    int coins[4] = {25, 10, 5, 1};
+
+    int coins_size = sizeof(coins) / sizeof(coins[0]);
+
+    // We check for every coin notation
+    for (int i = 0; i < coins_size; i++)
+    {
+        // If we can give it, do it!
+        if (change >= coins[i])
+        {
+            count += change / coins[i];
+            change -= (change / coins[i]) * coins[i];
+        }
+    }
+    return count;
+}
+
+int main(void)
+{
+    float change = 0;
+
+    // Do it indefinitely
+    while (change <= 0)
+    {
+        // Ask the change owed
+        change = get_float("Change owed: ");
+    }
+
+    change = round(change * 100.0);
+    int change_int = change;
+
+    int minimo = change_counter(change_int);
+
+    printf("%i\n", minimo);
+}
