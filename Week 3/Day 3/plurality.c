dd18688451094424237f0ae4be29471409c16edc
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-04-01 21:30:30 -0500

    Clean up plurality

--- Week 3/Day 3/plurality.c
+++ Week 3/Day 3/plurality.c
@@ -83,7 +83,6 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    string winners[candidate_count];
     int max_votes = 0;
 
     // Check the maximum
@@ -95,23 +94,11 @@ void print_winner(void)
         }
     }
 
-    // Check if there's a tie
-    string win_name = "";
-    int win_count = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == max_votes && strcmp(candidates[i].name, win_name) != 0)
+        if (candidates[i].votes == max_votes)
         {
-            winners[win_count] = candidates[i].name;
-            win_name = candidates[i].name;
-            win_count++;
+            printf("%s\n", candidates[i].name);
         }
     }
-
-    for (int i = 0; i < win_count; i++)
-    {
-        printf("%s\n", winners[i]);
-    }
-
-    return;
 }
