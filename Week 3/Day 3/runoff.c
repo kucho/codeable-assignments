dfc4a41e2127d3446fac5469010421aba23b37af
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-04-01 21:46:00 -0500

    Clean up runoff

--- Week 3/Day 3/runoff.c
+++ Week 3/Day 3/runoff.c
@@ -145,7 +145,6 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-
     for (int i = 0; i < voter_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
@@ -158,10 +157,7 @@ void tabulate(void)
                 break;
             }
         }
-
     }
-
-    return;
 }
 
 // Print the winner of the election, if there is one
